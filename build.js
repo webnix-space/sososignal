edc2de4dfc7f596adbcfe564a39c3a107acee6bd
Webnix <sanibmalik850@gmail.com>
2026-07-19 13:15:34 +0530

    Create build.js

---
+++ build.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+let html = fs.readFileSync(path.join(__dirname, 'index.template.html'), 'utf8');
+
+const replacements = {
+  '__SOSO_API_KEY__': process.env.SOSO_API_KEY || '',
+  '__GROQ_API_KEY__': process.env.GROQ_API_KEY || '',
+  '__SODEX_API_KEY__': process.env.SODEX_API_KEY || '',
+  '__SODEX_API_SECRET__': process.env.SODEX_API_SECRET || ''
+};
+
+for (const [key, val] of Object.entries(replacements)) {
+  html = html.split(key).join(val);
+}
+
+fs.writeFileSync(path.join(__dirname, 'index.html'), html);
+console.log('Build complete: API keys injected from env vars');
